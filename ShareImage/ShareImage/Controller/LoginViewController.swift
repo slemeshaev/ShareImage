ec08b31de23c6ee5f783d9122fe853022222e6af
Stanislav <lemeshaevs@gmail.com>
2020-10-18 15:52:19 +0300

    Сделал поля для логина и пароля и кнопку войти

--- ShareImage/ShareImage/Controller/LoginViewController.swift
+++ ShareImage/ShareImage/Controller/LoginViewController.swift
@@ -9,21 +9,69 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    // создаем текстовое поле для email
+    let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Email"
+        textField.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        textField.borderStyle = .roundedRect
+        textField.font = UIFont.systemFont(ofSize: 14)
+        return textField
+    }()
+    
+    // создаем текстовое поле для пароля
+    let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Пароль"
+        textField.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        textField.borderStyle = .roundedRect
+        textField.font = UIFont.systemFont(ofSize: 14)
+        return textField
+    }()
+    
+    // создаем кнопку для входа в приложение
+    let signInButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Войти", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor(red: 149/255,
+                                         green: 204/255,
+                                         blue: 244/255,
+                                         alpha: 1)
+        button.layer.cornerRadius = 5
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // цвет фона
+        view.backgroundColor = .white
+        
+        configureViewComponents()
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // метод конфигурации view-компонентов
+    private func configureViewComponents() {
+        
+        let stackView = UIStackView(arrangedSubviews: [emailTextField, passwordTextField, signInButton])
+        
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.distribution = .fillEqually
+        
+        view.addSubview(stackView)
+        stackView.anchor(top: view.topAnchor,
+                         left: view.leftAnchor,
+                         bottom: nil,
+                         right: view.rightAnchor,
+                         paddingTop: 40,
+                         paddingLeft: 40,
+                         paddingBottom: 0,
+                         paddingRight: 40,
+                         width: 0,
+                         height: 140)
     }
-    */
 
 }
