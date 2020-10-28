23b3e174f5eafad35a2b3de08baa3fce187160e0
slemeshaev <lemeshaevs@gmail.com>
2020-10-28 23:10:33 +0300

    user header profile

---
+++ ShareImage/ShareImage/Controller/UserProfile/UserProfileHeader.swift
@@ -0,0 +1,12 @@
+//
+//  UserProfileHeader.swift
+//  ShareImage
+//
+//  Created by Станислав Лемешаев on 28.10.2020.
+//
+
+import UIKit
+
+class UserProfileHeader: UICollectionViewCell {
+    
+}
